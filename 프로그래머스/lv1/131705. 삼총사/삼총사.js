a172c36e7edc37dcb7a85f963c09747c4bc61649
Thomas <79496740+KimYoonSu97@users.noreply.github.com>
2023-08-01 22:22:14 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,18 +1,7 @@
 function solution(number) {
     var answer = 0;
-    // 1번째 학생을 기준으로... 
-    // 2,3 // 2,4// 2,5
-    // 3,4 // 3,5
-    // 4,5
 
-    // 2번째 학생을 기준으로 ...
-    // 3,4 // 3,5
-    // 4,5
-    
-    // 3번째 학생을 기준으로...
-    // 4,5
-    
-    
+        
     for( let i = 0 ; i<number.length; i++){
         for(let j = i+1 ; j<number.length; j++){
             for(let y = j+1; y<number.length; y++){
