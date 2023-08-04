662911a2ce53008b33a9691ae725a7a4cb63c883
Thomas <79496740+KimYoonSu97@users.noreply.github.com>
2023-08-04 11:14:36 +0900

    [level 0] Title: 공 던지기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,34 +1,23 @@
 function solution(numbers, k) {
-  var answer = 0;
-
-  answer = numbers[k * 2];
-  let index = 0;
-  //던질 횟수
-  for (let i = 1; i <= k; i++) {
-    //만약 던질사람의 인덱스가 numbers의 length보다 크면??\
-    //렝스는 6
-    //2
-    //4
-    //6
-    //0
-    //2
-    if (index > numbers.length) {
-      index = index - numbers.length;
+    var answer = 0;
+    //i가 던지는 횟수임 k가 될때까지 던지는것임
+    let index = 0;
+    
+    for(let i = 1; i <= k; i++ ){
+        
+        //answer은 number배열의 [i+2] 번째로 고정
+        //근데이제... [i+2]가 numbers.length보다 크면 [i+2-numbers.length]를 해야함
+        
+        index = index + 2
+        
+        // console.log(index)
+        if(index > numbers.length) {
+            index = index%numbers.length
+        }
+        
+        answer = numbers[index]
+        
     }
-    //던진사람의 인덱스
-    answer = numbers[index];
-    //0
-    //2
-    //4
-    //6
-    //8
-    //받은사람의 인덱스 = 다음에 던질사람
-    index = index + 2;
-    //2
-    //4
-    //6
-    //8
-    //10
-  }
-  return answer;
+    
+    return answer;
 }
