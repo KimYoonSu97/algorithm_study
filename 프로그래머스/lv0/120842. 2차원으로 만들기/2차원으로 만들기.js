a56d16870d45d125884bf03cb42733f35b224fac
Thomas <79496740+KimYoonSu97@users.noreply.github.com>
2023-08-04 09:34:36 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,10 +1,4 @@
 function solution(num_list, n) {
-    var answer = [];
-
-    for(let i = 1; i <= num_list.length/n; i++){
-        answer.push(num_list.slice((n*i)-n,n*i))
-    }
-    
-    
+    var answer = [[]];
     return answer;
 }
