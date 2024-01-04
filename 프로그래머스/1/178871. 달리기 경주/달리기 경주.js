9d297724d50d3ec7b924fc75b29831b5dcbafd56
Thomas <79496740+KimYoonSu97@users.noreply.github.com>
2024-01-04 15:50:53 +0900

    [level 1] Title: 달리기 경주, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -5,29 +5,32 @@
         
         var answer = players
         const rank = {};
+//         이름이 키값이고 등수가 밸류인 객체 생성
         players.forEach((item,index)=>{
             return rank[item] = index
         })
         
+//         선수호명 수 만큼 반복
         for(let i = 0 ; i<callings.length; i++){
             
+//             불린 선수의 위치값
             let call = rank[callings[i]]
+//             불린 선수의 전 선수
             let temp = players[call-1]
             
+//             매개변수 배열의 위치값 -1 에는 불린선수 이름을 calling에서 가져다 넣음
             players[call-1] = callings[i];
-        players[call] = temp;
+//             그리고 불린선수 위치에는 전 선수 이름 넣음
+            players[call] = temp;        
             
-             rank[callings[i]] = call - 1;
-        rank[temp] = call;
+//             객체에도 해당 값 업데이트
+//             rank의 불린사람의 밸류값은 위치값에서 -1
+            rank[callings[i]] = call - 1;
+//             그리고 원ㄹ ㅐ사람은 불린선수 위치값 넣어주기
+            rank[temp] = call;
             
         }
-        
-    //     for(let i = 0; i<callings.length; i++){            
-    // const currentIndex = answer.indexOf(callings[i])
-    // const currentPlayer = answer[currentIndex]
-    // answer[currentIndex] = answer[currentIndex -1]
-    // answer[currentIndex -1] = currentPlayer
-    //     }
+
         
     return answer;
 }
